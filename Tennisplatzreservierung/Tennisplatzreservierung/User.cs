18637e84296e2b0dd1bf42a9c250960ca642e9c8
Paul Luger <paul.luger@gmx.at>
2019-02-27 08:50:50 +0100

    Salt programmiert

--- Tennisplatzreservierung/Tennisplatzreservierung/User.cs
+++ Tennisplatzreservierung/Tennisplatzreservierung/User.cs
@@ -1,96 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tennisplatzreservierung
+{
+    class User
     {
-        class User
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string password;
+        private string salt;
+        private int rank;
+        private string gender;
+        private string skillLevel;
+        private int userID;
+        private int age;
+
+        public string FirstName
         {
-            private string firstName;
-            private string lastName;
-            private string email;
-            private string password;
-            private string salt;
-            private int rank;
-            private string gender;
-            private string skillLevel;
-            private int userID;
-            private int age;
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ApplicationException("Vorname ist leer!");
+                else
+                    firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ApplicationException("Nachname ist leer!");
+                else
+                    lastName = value;
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ApplicationException("E-Mail ist leer!");
+                }
 
-            public string FirstName
-            {
-                get { return firstName; }
-                set
+                if (value.Contains("@"))
                 {
-                    if (string.IsNullOrEmpty(value))
-                        throw new ApplicationException("Vorname ist leer!");
-                    else
-                        firstName = value;
+                    string[] splitEmail = value.Split('@');
+                    if (splitEmail[2].Contains("."))
+                        email = value;
+                }
+                else
+                {
+                    throw new ApplicationException("E-Mail ist nicht gültig!");
                 }
             }
-            public string LastName
+        }
+        public string Password
+        {
+            get { return password; }
+            set
             {
-                get { return lastName; }
-                set
-                {
-                    if (string.IsNullOrEmpty(value))
-                        throw new ApplicationException("Nachname ist leer!");
-                    else
-                        lastName = value;
-                }
-            }
-            public string Email
-            {
-                get { return email; }
-                set
-                {
-                    if (string.IsNullOrEmpty(value))
-                    {
-                        throw new ApplicationException("E-Mail ist leer!");
-                    }
 
-                    if (value.Contains("@"))
-                    {
-                        string[] splitEmail = value.Split('@');
-                        if (splitEmail[2].Contains("."))
-                            email = value;
-                    }
-                    else
-                    {
-                        throw new ApplicationException("E-Mail ist nicht gültig!");
-                    }
-                }
             }
-            public string Password
-            {
-                get { return password; }
-                set
-                {
-
-                }
-            }
-            public string Salt { get { return salt; } set { } }
-            public int Rank
-            {
-                get { return rank; }
-                set
-                {
-                    if (value < 0 || value > 2)
-                        throw new ApplicationException("Mögliche Ränge sind 0...nicht verifiziert 1...Mitglied 2...Admin!");
-                    else
-                        rank = value;
-                }
-            }
-            public string Gender { get { return gender; } set { } }
-            public string SkillLevel { get { return skillLevel; } set { } }
-            public int Age { get { return age; } set { } }
-            public int UserID { get { return userID; } set { } }
-
         }
 
+        private static string CreatePasswordHash(string pwd, string salt)
+        {
+          
+        }
+        public string Salt { get { return salt; } set { } }
+
+        private static string CreateSalt(int size)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+
+            return Convert.ToBase64String(buff);
+
+        }
+        public int Rank
+        {
+            get { return rank; }
+            set
+            {
+                if (value < 0 || value > 2)
+                    throw new ApplicationException("Mögliche Ränge sind 0...nicht verifiziert 1...Mitglied 2...Admin!");
+                else
+                    rank = value;
+            }
+        }
+        public string Gender { get { return gender; } set { } }
+        public string SkillLevel { get { return skillLevel; } set { } }
+        public int Age { get { return age; } set { } }
+        public int UserID { get { return userID; } set { } }
+
     }
+
+}
 
 }
 
